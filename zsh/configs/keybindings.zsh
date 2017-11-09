c8703f07ac519015743736ebc8496d6dab53f8db
Arumoy Shome <arumoy.shome.work@gmail.com>
2017-11-09 14:11:26 -0500

    Remove conflicting keybindings.
    These need to be removed since fzf adds functionality to them and I want
    to give fzf priority.

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -8,6 +8,3 @@ bindkey "^F" vi-cmd-mode
 # handy keybindings
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
-bindkey "^T" history-incremental-search-backward
-bindkey "^P" history-search-backward
-bindkey "^N" history-search-forward
