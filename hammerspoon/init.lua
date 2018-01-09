8fd3362807d609d89b47b49e9192c6097442baae
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-01-08 21:57:55 -0500

    Basic Hammerspoon init file.
    Basic init file which disables window animations and allows to move
    windows either left or right.

---
+++ hammerspoon/init.lua
@@ -0,0 +1,30 @@
+-- disable animations
+hs.window.animationDuration = 0
+
+-- tile window to left
+hs.hotkey.bind({"cmd", "ctrl"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- tile window to right
+hs.hotkey.bind({"cmd", "ctrl"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
