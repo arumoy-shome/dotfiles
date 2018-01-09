d878cf2c4cc0c4c88022e4ae3eae8fe053453ead
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-01-08 20:39:27 -0500

    Use the default-dark theme by default.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,4 @@
 # Base16 Shell
 if status --is-interactive
-    eval sh $HOME/.config/base16-shell/scripts/base16-tomorrow-night.sh
+    eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
 end
