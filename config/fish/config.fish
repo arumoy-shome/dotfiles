6b1189db09a88fda46bb48f8fe34980d17d6b0f6
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-01-16 22:56:12 -0500

    Define some handy abbreviations in fish.
    Alternative to alias, I quite like the fish abbreviations as it adds a nice look
    when I am tying. Also I am able see the entire command that I am about to run
    which helps when I am passing flags or arguments.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,3 +5,28 @@ end
 
 # load ~/.bin for personal commands
 set PATH $HOME/.bin $PATH
+
+# define some useful abbreviations
+# abbreviations are like alias but with nicer gui
+if status --is-interactive
+  set -g fish_user_abbreviations
+  abbr -a b "brew"
+  abbr -a bc "brew cask"
+  abbr -a bci "brew cask install"
+  abbr -a bcs "brew cask search"
+  abbr -a bi "brew install"
+  abbr -a bs "brew search"
+  abbr -a cl "clear"
+  abbr -a g "git"
+  abbr -a gco "git checkout"
+  abbr -a gco "git checkout"
+  abbr -a gfm "git pull origin"
+  abbr -a gia "git add"
+  abbr -a gp "git push origin"
+  abbr -a gwd "git diff"
+  abbr -a gws "git status"
+  abbr -a jp "jupyter"
+  abbr -a pip "pip3"
+  abbr -a py "python3"
+  abbr -a t "tmux"
+end
