a54318d65fefaaa8863338ef7883f2b161af9bd9
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-02-17 22:22:50 -0500

    Use tomorrow-night for shell
    it's hard to tell if I am in emacs or the shell, especially when one is on
    top of another.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,6 @@
 # Base16 Shell
 if status --is-interactive
-    eval sh $HOME/.config/base16-shell/scripts/base16-default-dark.sh
+    eval sh $HOME/.config/base16-shell/scripts/base16-tomorrow-night.sh
 end
 
 # load ~/.bin for personal commands
