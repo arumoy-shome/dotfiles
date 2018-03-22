154bbd2bd21508ffb2cfa95f278667a450d68371
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-03-22 19:22:22 -0400

    Move personal config to private layer.
    Maintaining my own fork was causing me to miss out on features being introduced
    by breaking changes (as my version kept overriding the most recent commit from
    the maintainer).

--- config/doom/config.el
+++ config/doom/config.el
@@ -1,3 +1,15 @@
 ;;; private/aru/config.el -*- lexical-binding: t; -*-
 
+;; ui
+(setq doom-theme 'doom-citylights)
+(setq doom-font (font-spec :family "Source Code Pro" :size 13 :weight 'light))
+(setq doom-big-font (font-spec :family "Source Code Pro" :size 15 :weight 'normal))
+(setq doom-line-numbers-style nil)
+(setq +doom-dashboard-functions '(doom-dashboard-widget-shortmenu
+                                  doom-dashboard-widget-loaded))
+
+;; org
+(setq org-ellipsis " ▼ ")
+
+;; editing
 (add-hook! text-mode 'auto-fill-mode)
