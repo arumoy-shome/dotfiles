6696436f259cb6ce50d72bff11591e0297030ec7
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-04-02 01:34:55 -0400

    Hammerspoon cleanup and window manager
    Remove unused Spoons and use a window manager instead of writing my own
    config.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,48 +1,12 @@
--- disable animations
+local hyper = {"ctrl", "cmd"}
+
+hs.loadSpoon("MiroWindowsManager")
+
 hs.window.animationDuration = 0
-
--- tile window to left
-hs.hotkey.bind({"cmd", "ctrl"}, "H", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
--- tile window to right
-hs.hotkey.bind({"cmd", "ctrl"}, "L", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end)
-
--- make window fullscreen
-hs.hotkey.bind({"cmd", "ctrl"}, "return", function()
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x
-    f.y = max.y
-    f.w = max.w
-    f.h = max.h
-    win:setFrame(f)
-end)
-
--- auto reload config spoon
-hs.loadSpoon("ReloadConfiguration")
-spoon.ReloadConfiguration:start()
+spoon.MiroWindowsManager:bindHotkeys({
+  up = {hyper, "up"},
+  right = {hyper, "right"},
+  down = {hyper, "down"},
+  left = {hyper, "left"},
+  fullscreen = {hyper, "f"}
+})
