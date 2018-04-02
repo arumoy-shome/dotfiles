d9979867bcda2c4156568bd698ead1925f64fd2d
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-04-02 01:42:20 -0400

    The heart wants what it wants

--- config/doom/config.el
+++ config/doom/config.el
@@ -1,7 +1,7 @@
 ;;; private/aru/config.el -*- lexical-binding: t; -*-
 
 ;; ui
-(setq doom-theme 'doom-citylights)
+(setq doom-theme 'doom-tomorrow-night)
 (setq doom-font (font-spec :family "Source Code Pro" :size 13 :weight 'light))
 (setq doom-big-font (font-spec :family "Source Code Pro" :size 15 :weight 'normal))
 (setq doom-line-numbers-style nil)
