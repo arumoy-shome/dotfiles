f216e4bfa5b4f1296f3079efe86a067097e2f418
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-10-23 02:51:35 +0200

    fish/functions/prompt: indicate if jobs present

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,10 +2,14 @@ function fish_prompt
     echo
     set last_status $status
 
-    test $last_status -eq 0; and set -l suffix (set_color green)'❯'
-    test $last_status -eq 1; and set -l suffix (set_color red)'❯'
+    # redirect output to stdout and stderror to /dev/null
+    # taken from fish documentation>syntax overview>i/o redirection
+    jobs > /dev/null ^&1; and set -l job (set_color blue)'❯'
+    test $last_status -eq 0; and set suffix (set_color white)'❯'
+    test $last_status -eq 1; and set suffix (set_color red)'❯'
 
     printf '%s' (set_color -i $fish_color_cwd)(basename (pwd))
     echo
+    printf '%s' $job
     printf '%s ' $suffix
 end
