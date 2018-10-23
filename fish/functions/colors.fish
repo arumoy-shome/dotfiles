e5d552d0d54aff928290a013885f70b60de498d1
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-10-23 16:23:19 +0200

    fish/colors: refactor
    + fix issue where _list_colors was returning the substring and the
    original string
    + and other general cleanup

--- fish/functions/colors.fish
+++ fish/functions/colors.fish
@@ -1,35 +1,21 @@
-set base "$HOME/.config/base16-shell/scripts/"
-
-function colors
-  if _is_interactive; and _valid_color $argv
-    eval sh $HOME/.config/base16-shell/scripts/base16-$argv.sh
-    echo "theme set successfully!"
-  else
-    _list_colors
-    echo 'invalid scheme selection, please choose a theme from above...'
-  end
-end
+set base "$BASE16_SHELL/scripts/"
 
 function _list_colors
-  set options (ls $base)
-
-  for option in $options
-    string match -r 'base16-(.*).sh' $option
-  end
-end
-
-function _is_interactive
-  status --is-interactive
+    # look after '-' and before '.'
+    string match -r '(?<=-)[\S\d]*(?=\.)' (ls $base)
 end
 
 function _valid_color
-  set options (_list_colors)
+  set colors (_list_colors)
 
-  for option in $options
-    if string match "$argv" $option
-      break
-    else
-      false
-    end
+  contains $argv $colors
+end
+
+function colors
+  if _valid_color $argv
+    eval base16-$argv
+    echo "theme set successfully!"
+  else
+    _list_colors
   end
 end
