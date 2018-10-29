f6d99d8f62a35c6b096b0c9dcbb079ebe27dab72
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-10-29 23:54:39 +0100

    pyenv: simplify installation

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,3 @@
 set fish_greeting 'welcome, '(whoami)'.'
 
 abbr -a jp "jupyter"
-abbr -a py "python3"
-abbr -a py2 "python2"
-abbr -a ipy "ipython3"
--- pyenv/pyenv.fish
+++ pyenv/pyenv.fish
@@ -1,16 +1,4 @@
-set -gx PATH '/Users/aru/.pyenv/shims' $PATH
-set -gx PYENV_SHELL fish
+status --is-interactive; and source (pyenv init -|psub)
 
-source '/Users/aru/.config/pyenv/libexec/../completions/pyenv.fish'
-command pyenv rehash 2>/dev/null
-function pyenv
-    set command $argv[1]
-    set -e argv[1]
-
-    switch "$command"
-        case rehash shell
-            source (pyenv "sh-$command" $argv|psub)
-        case '*'
-            command pyenv "$command" $argv
-    end
-end
+abbr -a py "python"
+abbr -a ipy "ipython"
