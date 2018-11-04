527c419d3c1454f8a342d6f0ae4250026d44927a
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-11-04 02:08:24 +0100

    fish/function/fish_prompt: no italics
    looks better just straight.
    
    Co-authored-by: shrutirao94 <shrutirao94@gmail.com>

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -8,7 +8,7 @@ function fish_prompt
     test $last_status -eq 0; and set suffix (set_color white)'❯'
     test $last_status -eq 1; and set suffix (set_color red)'❯'
 
-    printf '%s' (set_color -i $fish_color_cwd)(basename (pwd))
+    printf '%s' (set_color $fish_color_cwd)(basename (pwd))
     echo
     printf '%s' $job
     printf '%s ' $suffix
