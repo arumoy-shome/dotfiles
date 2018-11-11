2cc33eb16cb2ffe84d635b132ccc1de59de5341f
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-11-11 20:04:52 +0100

    base: set abbreviations to global scope
    by default, fish sets the scope of all abbreviations to universal. this
    results in a dirty uninstall of topics (abbreviations of topics are left
    behind if not manually removed). the fix is to set
    fish_user_abbreviations to the global scope before the first
    abbreviation.
    
    fish evaluates files under conf.d in alphabetical order. as such,
    base.fish is the first file that is evaulated by fish.

--- base/base.fish
+++ base/base.fish
@@ -1,6 +1,6 @@
+# fish evaulates files in conf.d before config.fish
+# this is the first file that is evaluated in conf.d
+# thus we set fish_user_abbreviations at global scope here
+set -g fish_user_abbreviations
 abbr -a b "brew"
 abbr -a bc "brew cask"
-abbr -a bci "brew cask install"
-abbr -a bcs "brew cask search"
-abbr -a bi "brew install"
-abbr -a bs "brew search"
