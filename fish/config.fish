75829407cff26c63a1ace456b320c41a2cec7670
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-12-03 00:37:31 +0100

    fish/config: export xdg variables
    also set a new global variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,10 @@
 # change the greeting message
 set fish_greeting 'welcome, '(whoami)'.'
 
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+
+set -g XDG_DATA_BIN "$HOME/.local/bin"
+set -g FISH_CONFIG_HOME "$XDG_CONFIG_HOME/fish/conf.d"
+
 abbr -a jp "jupyter"
