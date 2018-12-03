14cc94da30319e11a65864743a95218560f29afd
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-12-04 00:11:23 +0100

    fish/config: add XDG_DATA_BIN to PATH
    XDG_DATA_BIN (~/.local/bin) is where I store all my executables so this
    needs to be in the PATH.

--- fish/config.fish
+++ fish/config.fish
@@ -7,4 +7,6 @@ set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -g XDG_DATA_BIN "$HOME/.local/bin"
 set -g FISH_CONFIG_HOME "$XDG_CONFIG_HOME/fish/conf.d"
 
+set PATH $XDG_DATA_BIN $PATH
+
 abbr -a jp "jupyter"
