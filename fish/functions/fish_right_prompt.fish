318b6dad5b4f381387478ebce8f05d74b355eb4d
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-12-04 02:31:30 +0100

    fish/fish_right_prompt: improvements
    richer git status indicator

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -2,5 +2,15 @@ function fish_right_prompt
     set -g __fish_git_prompt_describe_style default
     set -g __fish_git_prompt_showcolorhints true
     set -g __fish_git_prompt_showdirtystate true
+    set -g __fish_git_prompt_showuntrackedfiles true
+    set -g __fish_git_prompt_showupstream auto informative
+
+    set -g __fish_git_prompt_char_dirtystate ●
+    set -g __fish_git_prompt_char_stagedstate ●
+    set -g __fish_git_prompt_char_untrackedfiles ●
+    set -g __fish_git_prompt_char_invalidstate ✖
+    set -g __fish_git_prompt_char_upstream_ahead ↑
+    set -g __fish_git_prompt_char_upstream_behind ↓
+    set -g __fish_git_prompt_char_upstream_diverged ↓↑
     printf '%s' (__fish_git_prompt)
 end
