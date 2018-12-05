e63d99d565a665cfa7c0b73c731959fa3c96a8ef
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-12-05 19:29:12 +0100

    fish/functions/colors: update ./vim/.background
    vim looks for this file and loads it so that the colorscheme is set
    automatically.

--- fish/functions/colors.fish
+++ fish/functions/colors.fish
@@ -2,6 +2,10 @@ function colors -d "switch to a dark of light theme"
     set base "$XDG_DATA_HOME/base16"
     set color $argv[1]
 
-    test "$color" = "light"; and sh "$base/base16-solarized-light.sh"
-    test "$color" = "dark"; and sh "$base/base16-tomorrow-night.sh"
+    test "$color" = "light"; and set theme "base16-solarized-light"
+    test "$color" = "dark"; and set theme "base16-tomorrow-night"
+
+    sh "$base/$theme.sh"
+
+    echo -e "if !exists('g:colors_name') || g:colors_name != '$theme'\n  colorscheme $theme\nendif" >  ~/.vim/.background
 end
