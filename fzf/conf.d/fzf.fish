75faf85ab8c8f9d1ee018937e35991005088f603
Arumoy Shome <arumoy.shome.work@gmail.com>
2018-12-18 01:59:55 +0100

    fzf: export default fzf command

--- fzf/conf.d/fzf.fish
+++ fzf/conf.d/fzf.fish
@@ -1,3 +1,4 @@
 # use fd instead of find
 # depends on fd (https://github.com/sharkdp/fd)
 set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
