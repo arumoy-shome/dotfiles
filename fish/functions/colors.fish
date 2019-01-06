6b394da7f51800cbdeff7b94b6dabdd362b33879
Andrew Malcolmson <andmalc@gmail.com>
2019-01-06 18:27:34 -0500

    Several changes to color.fish
    Hi - saw your post on Reddit.  This is my first PR so kind of doing this as practice.  I hope it's helpful.
      
    1. Added a test to set $base to a hard coded path if $XDG_DATA_HOME is not set.  This is the case on my Fedora system. 
    2. The Base16 theme scripts are in the 'scripts' subdirectory so inserted that in path.
    3. Vim scripts need .vim at the end.  Also they don't seem to get sourced unless in a subdirectory of ~/.vim.  Changed the last line accordingly.

--- fish/functions/colors.fish
+++ fish/functions/colors.fish
@@ -1,11 +1,16 @@
-function colors -d "switch to a dark of light theme"
-    set base "$XDG_DATA_HOME/base16"
+function colors --description 'switch to a dark of light theme'
+    if set -q XDG_DATA_HOME
+        set base "$XDG_DATA_HOME/base16"
+    else
+        set base "$HOME/.config/base16-shell"
+    end
+    
     set color $argv[1]
 
     test "$color" = "light"; and set theme "base16-solarized-light"
     test "$color" = "dark"; and set theme "base16-tomorrow-night"
 
-    sh "$base/$theme.sh"
+    sh "$base/scripts/$theme.sh"
 
-    echo -e "if !exists('g:colors_name') || g:colors_name != '$theme'\n  colorscheme $theme\nendif" >  ~/.vim/.background
+    echo -e "if !exists('g:colors_name') || g:colors_name != '$theme'\n  colorscheme $theme\nendif" >  ~/.vim/plugin/background.vim
 end
