658fa46b8a90ab743223ad37f75a533212654a0c
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-02-03 22:13:12 +0100

    fish/config: kill base16
    the base16 scripts injects weird escape sequences into minibuffer when
    using ag through helm and in many other situations.

--- fish/config.fish
+++ fish/config.fish
@@ -9,9 +9,5 @@ set -g FISH_CONFIG_HOME "$XDG_CONFIG_HOME/fish/conf.d"
 
 set PATH $XDG_DATA_BIN $PATH
 
-if not test "$TERM" = "eterm-color"
-    colors dark
-end
-
 abbr -a jp "jupyter"
 set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
