385d2721d609557d3bac158f574e12cb9f36b69a
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-02-04 00:45:35 +0100

    fish/prompt: remove unneccessary settings
    the prompt does not change based on the last status so it is best
    removed for now.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,14 +1,12 @@
 function fish_prompt
     echo
-    set last_status $status
-
+    
     # redirect output to stdout and stderror to /dev/null
     # taken from fish documentation>syntax overview>i/o redirection
-    jobs > /dev/null ^&1; and set job (set_color blue)'❯'
-    test $last_status -eq 0; and set suffix (set_color white)'❯'
-    test $last_status -eq 1; and set suffix (set_color red)'❯'
+    jobs > /dev/null ^&1; and set job (set_color blue)'❯'(set_color normal)
+    set suffix (set_color white)'❯'(set_color normal)
 
-    printf '%s ' (set_color $fish_color_cwd)(basename (pwd))
+    printf '%s ' (set_color $fish_color_cwd)(basename (pwd))(set_color normal)
     printf '%s' $job
     printf '%s ' $suffix
 end
