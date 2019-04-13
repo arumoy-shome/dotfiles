0d4731b24f664b10bde8f07d3422e9ee101b559d
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-04-14 00:14:49 +0200

    [hammerspoon]: smart sync
    Google Backup and Sync is a resource hog. I use hammerspoon to watch
    for changes in the directories that that I sync and run sync for only 5
    minutes.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,6 @@
 local hyper = {"ctrl", "cmd", "alt", "shift"}
 
+-- window manager
 hs.loadSpoon("MiroWindowsManager")
 
 hs.window.animationDuration = 0
@@ -11,7 +12,27 @@ spoon.MiroWindowsManager:bindHotkeys({
   fullscreen = {hyper, "f"}
 })
 
+-- reload
 hs.hotkey.bind(hyper, "R", function()
   hs.reload()
 end)
 hs.alert.show("Config loaded")
+
+-- watchers
+function startSync(files)
+  local gdrive = hs.application.find("Backup and Sync")
+
+  if not gdrive then
+    hs.application.launchOrFocus("Backup and Sync")
+    -- wait for 5 minutes and then quit sync
+    hs.timer.delayed.new(300, stopSync):start()
+  end
+end
+
+function stopSync()
+  hs.application.find("Backup and Sync"):kill()
+end
+
+local gdriveWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/Google Drive", startSync):start()
+local orgWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/org", startSync):start()
+local documentsWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/Documents", startSync):start()
