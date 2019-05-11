e305c908c6c8dbebbec7f9ad7d0b0ae094528a4c
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-05-11 14:40:36 +0200

    [fish]: create activate()
    Python virtualenv provides a convenient deactivate function to exit out
    of a virtualenv. The activate() function is meant to mirror that. It
    looks for common virtualenv names and calls the activate script.

---
+++ fish/functions/activate.fish
@@ -0,0 +1,17 @@
+function activate -d "mirror the deactivate function provided by virtualenv"
+    # fish does not allow command substitution in lists longer than 1
+    # so we have so save the result of the command first
+    set dir_basename (basename (pwd))
+    set venv_names "env/" "venv/" "virtualenv/" "$dir_basename/"
+    set listing (ls -d */ (pwd)) # list only directories
+
+    for name in $venv_names
+      contains $name $listing; and set venv_basename $name
+    end
+
+    if test -n "$venv_basename"
+        source $venv_basename/bin/activate.fish
+    else
+        echo "No virtualenv available in this directory."
+    end
+end
