57556908f81c1cb9262d2887730e1dbf2927326e
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-08-23 15:30:53 +0530

    vim: use command-T instead of fzf
    Fzf uses tmux panes to display the results, however with my current
    setup of setting tmux styles on the fly, fzf doesn't work properly
    anymore.

--- nvim/conf.d/nvim.fish
+++ nvim/conf.d/nvim.fish
@@ -2,4 +2,8 @@ set -gx EDITOR nvim
 set -gx VISUAL $EDITOR
 set -gx MANPAGER "nvim --noplugin -Rc 'set ft=man' -"
 
+# add the path to ruby gems installed by brew so that neovim can locate the ruby
+# provider
+set PATH /usr/local/lib/ruby/gems/2.6.0/bin /usr/local/opt/ruby/bin $PATH
+
 abbr -a v "nvim"
---
+++ vim/plugin/commandt.vim
@@ -0,0 +1,4 @@
+nmap <silent> <C-p> <Plug>(CommandT)
+nmap <silent> <Leader>b <Plug>(CommandTBuffer)
+nmap <silent> <Leader>j <Plug>(CommandTJump)
+nmap <silent> <Leader>h <Plug>(CommandTHelp)
