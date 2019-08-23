b0a57cde891a2c69a767db6b6a28face490b79cc
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-08-23 12:11:59 +0530

    fish: export LC_ALL
    This fixes the "failed to set locale category LC_*" errors when starting
    vim.

--- fish/config.fish
+++ fish/config.fish
@@ -7,4 +7,6 @@ set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -g XDG_DATA_BIN "$HOME/.local/bin"
 set -g FISH_CONFIG_HOME "$XDG_CONFIG_HOME/fish/conf.d"
 
+set -x LC_ALL en_US.UTF-8
+
 set PATH $XDG_DATA_BIN $PATH
