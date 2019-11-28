770316510cd08208c2a7bbd305e54da522e1dfc2
Arumoy Shome <arumoy.shome.work@gmail.com>
2019-11-28 20:16:55 +0100

    [hammerspoon]: 50-50 layout
    A quick binding for a 50-50 layout with my terminal and browser.

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -22,15 +22,17 @@ spoon.MiroWindowsManager:bindHotkeys({
 
 -- send focused window to other desktop
 hs.hotkey.bind(hyper, "o", function()
-  -- Get the focused window, its window frame dimensions, its screen frame dimensions,
-  -- and the next screen's frame dimensions.
+  -- Get the focused window, its window frame dimensions, its screen frame
+  -- dimensions, and the next screen's frame dimensions.
   local focusedWindow = hs.window.focusedWindow()
   local focusedScreenFrame = focusedWindow:screen():frame()
   local nextScreenFrame = focusedWindow:screen():next():frame()
   local windowFrame = focusedWindow:frame()
 
-  -- Calculate the coordinates of the window frame in the next screen and retain aspect ratio
-  windowFrame.x = ((((windowFrame.x - focusedScreenFrame.x) / focusedScreenFrame.w) * nextScreenFrame.w) + nextScreenFrame.x)
+  -- Calculate the coordinates of the window frame in the next screen and retain
+  -- aspect ratio
+  windowFrame.x = ((((windowFrame.x - focusedScreenFrame.x) /
+  focusedScreenFrame.w) * nextScreenFrame.w) + nextScreenFrame.x)
   windowFrame.y = ((((windowFrame.y - focusedScreenFrame.y) / focusedScreenFrame.h) * nextScreenFrame.h) + nextScreenFrame.y)
   windowFrame.h = ((windowFrame.h / focusedScreenFrame.h) * nextScreenFrame.h)
   windowFrame.w = ((windowFrame.w / focusedScreenFrame.w) * nextScreenFrame.w)
@@ -42,4 +44,16 @@ end)
 -- hotkey for terminal
 hs.hotkey.bind(hyper, "Return", function()
   hs.application.launchOrFocus("iTerm")
+end)
+
+-- editor and browser 50-50 layout
+hs.hotkey.bind(hyper, "1", function()
+  local mainScreen = "Color LCD"
+  local terminal = hs.application.find("iTerm")
+  local browser = hs.application.find("qutebrowser")
+  local layout = {
+    {terminal, nil, mainScreen, hs.layout.left50, nil, nil},
+    {browser, nil, mainScreen, hs.layout.right50, nil, nil}
+  }
+  hs.layout.apply(layout)
 end)
