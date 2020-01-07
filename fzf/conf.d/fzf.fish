8866391a27798628d85d67c6db6a3bfdf39b458c
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-01-07 22:18:47 +0100

    [fzf]: add guard before exporting default command
    Check if fd is installed before using it as default command for fzf.

--- fzf/conf.d/fzf.fish
+++ fzf/conf.d/fzf.fish
@@ -1,4 +1,5 @@
-# use fd instead of find
-# depends on fd (https://github.com/sharkdp/fd)
-set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
-set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+# use fd instead of find if available
+if command -sq fd
+  set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+  set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+end
