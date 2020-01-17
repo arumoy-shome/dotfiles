7d3c26982fbad77430a6b467c40076c5c954297f
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-01-17 16:50:40 +0100

    [fish]: central path manipulation
    noticed a bug where $PATH had repeated values. This is because
    config.fish and other files in conf.d/ were manipulating $PATH, since
    fish sources these files at startup, opening a tmux session sources
    these files twice. Fix is the centralize all path manipulation in one
    file and set $PATH only if tmux is not running.

---
+++ fish/conf.d/path.fish
@@ -0,0 +1,8 @@
+# NOTE: fish sources config.fish all files under conf.d on startup, thus they
+# get sourced twice if we launch tmux (once when we lauch the terminal and then
+# again when we lauch a tmux session).
+
+if test -z "$TMUX"
+  set PATH $HOME/.local/bin $PATH # add $HOME/.local/bin containing local scripts
+  set PATH /usr/local/lib/ruby/gems/2.6.0/bin /usr/local/opt/ruby/bin $PATH # required by nvim
+end
--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,7 @@
 # change the greeting message
 set fish_greeting 'welcome, '(whoami)'.'
-
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
-
 set -g XDG_DATA_BIN "$HOME/.local/bin"
 set -g FISH_CONFIG_HOME "$XDG_CONFIG_HOME/fish/conf.d"
-
 set -x LC_ALL en_US.UTF-8
-
-set PATH $XDG_DATA_BIN $PATH
