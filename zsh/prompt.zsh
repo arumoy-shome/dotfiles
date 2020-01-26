dc3293f6352f42c97242365e9d8095570df02c97
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-01-26 01:09:15 +0100

    Revert "[zsh]: use pure prompt."
    This reverts commit 69b668b1a16685bea65e2505d46d649d8753f6b6.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,71 @@
-if [[ -d "$ZDOTDIR/pure" ]]; then
-  fpath+=("$ZDOTDIR/pure")
-  autoload -U promptinit; promptinit
-  prompt pure
-fi
+# make color constants available
+autoload -U colors
+colors
+
+# http://zsh.sourceforge.net/Doc/Release/User-Contributions.html
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' stagedstr "%F{green}●%f" # default 'S'
+zstyle ':vcs_info:*' unstagedstr "%F{red}●%f" # default 'U'
+zstyle ':vcs_info:*' use-simple true
+zstyle ':vcs_info:git+set-message:*' hooks git-untracked
+zstyle ':vcs_info:git*:*' formats '[%b%m%c%u] ' # default ' (%s)-[%b]%c%u-'
+zstyle ':vcs_info:git*:*' actionformats '[%b|%a%m%c%u] ' # default ' (%s)-[%b|%a]%c%u-'
+
+# function +vi-hg-bookmarks() {
+#   emulate -L zsh
+#   if [[ -n "${hook_com[hg-active-bookmark]}" ]]; then
+#     hook_com[hg-bookmark-string]="${(Mj:,:)@}"
+#     ret=1
+#   fi
+# }
+
+# function +vi-hg-message() {
+#   emulate -L zsh
+
+#   # Suppress hg branch display if we can display a bookmark instead.
+#   if [[ -n "${hook_com[misc]}" ]]; then
+#     hook_com[branch]=''
+#   fi
+#   return 0
+# }
+
+function +vi-git-untracked() {
+  emulate -L zsh
+  if [[ -n $(git ls-files --exclude-standard --others 2> /dev/null) ]]; then
+    hook_com[unstaged]+="%F{blue}●%f"
+  fi
+}
+
+RPROMPT_BASE="\${vcs_info_msg_0_}%F{blue}%~%f"
+setopt PROMPT_SUBST
+
+# Anonymous function to avoid leaking variables.
+function () {
+  # Check for tmux by looking at $TERM, because $TMUX won't be propagated to any
+  # nested sudo shells but $TERM will.
+  local TMUXING=$([[ "$TERM" =~ "tmux" ]] && echo tmux)
+  if [ -n "$TMUXING" -a -n "$TMUX" ]; then
+    # In a a tmux session created in a non-root or root shell.
+    local LVL=$(($SHLVL - 1))
+  else
+    # Either in a root shell created inside a non-root tmux session,
+    # or not in a tmux session.
+    local LVL=$SHLVL
+  fi
+  if [[ $EUID -eq 0 ]]; then
+    local SUFFIX='%F{yellow}%n%f'$(printf '%%F{yellow}\u276f%.0s%%f' {1..$LVL})
+  else
+    local SUFFIX=$(printf '%%F{red}\u276f%.0s%%f' {1..$LVL})
+  fi
+  export PS1="%F{green}${SSH_TTY:+%n@%m}%f%B${SSH_TTY:+:}%b%F{blue}%B%1~%b%F{yellow}%B%(1j.*.)%(?..!)%b%f %B${SUFFIX}%b "
+  if [[ -n "$TMUXING" ]]; then
+    # Outside tmux, ZLE_RPROMPT_INDENT ends up eating the space after PS1, and
+    # prompt still gets corrupted even if we add an extra space to compensate.
+    export ZLE_RPROMPT_INDENT=0
+  fi
+}
+
+export RPROMPT=$RPROMPT_BASE
+export SPROMPT="zsh: correct %F{red}'%R'%f to %F{red}'%r'%f [%B%Uy%u%bes, %B%Un%u%bo, %B%Ue%u%bdit, %B%Ua%u%bbort]? "
