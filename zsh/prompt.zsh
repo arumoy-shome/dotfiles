d419246a806004d9bd3363ee2882ed30c1186940
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-01-26 01:28:10 +0100

    [zsh]: restore original wincent prompt
    Although I doubt that is the cause of the outdated git prompt.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,32 +4,41 @@ colors
 
 # http://zsh.sourceforge.net/Doc/Release/User-Contributions.html
 autoload -Uz vcs_info
-zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' enable git hg
 zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' disable-patterns "${(b)HOME}/code/portal(|-ee)(|/*)"
 zstyle ':vcs_info:*' stagedstr "%F{green}●%f" # default 'S'
 zstyle ':vcs_info:*' unstagedstr "%F{red}●%f" # default 'U'
 zstyle ':vcs_info:*' use-simple true
 zstyle ':vcs_info:git+set-message:*' hooks git-untracked
 zstyle ':vcs_info:git*:*' formats '[%b%m%c%u] ' # default ' (%s)-[%b]%c%u-'
 zstyle ':vcs_info:git*:*' actionformats '[%b|%a%m%c%u] ' # default ' (%s)-[%b|%a]%c%u-'
+zstyle ':vcs_info:hg*:*' formats '[%m%b] '
+zstyle ':vcs_info:hg*:*' actionformats '[%b|%a%m] '
+zstyle ':vcs_info:hg*:*' branchformat '%b'
+zstyle ':vcs_info:hg*:*' get-bookmarks true
+zstyle ':vcs_info:hg*:*' get-revision true
+zstyle ':vcs_info:hg*:*' get-mq false
+zstyle ':vcs_info:hg*+gen-hg-bookmark-string:*' hooks hg-bookmarks
+zstyle ':vcs_info:hg*+set-message:*' hooks hg-message
 
-# function +vi-hg-bookmarks() {
-#   emulate -L zsh
-#   if [[ -n "${hook_com[hg-active-bookmark]}" ]]; then
-#     hook_com[hg-bookmark-string]="${(Mj:,:)@}"
-#     ret=1
-#   fi
-# }
+function +vi-hg-bookmarks() {
+  emulate -L zsh
+  if [[ -n "${hook_com[hg-active-bookmark]}" ]]; then
+    hook_com[hg-bookmark-string]="${(Mj:,:)@}"
+    ret=1
+  fi
+}
 
-# function +vi-hg-message() {
-#   emulate -L zsh
+function +vi-hg-message() {
+  emulate -L zsh
 
-#   # Suppress hg branch display if we can display a bookmark instead.
-#   if [[ -n "${hook_com[misc]}" ]]; then
-#     hook_com[branch]=''
-#   fi
-#   return 0
-# }
+  # Suppress hg branch display if we can display a bookmark instead.
+  if [[ -n "${hook_com[misc]}" ]]; then
+    hook_com[branch]=''
+  fi
+  return 0
+}
 
 function +vi-git-untracked() {
   emulate -L zsh
