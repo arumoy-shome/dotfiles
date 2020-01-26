ef02f966805a95bb3ba3e58712b7712c3e0bb25e
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-01-26 18:43:16 +0100

    [fish]: introduce FDOTDIR variable
    Inspired by ZDOTDIR.

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,5 @@ set fish_greeting 'welcome, '(whoami)'.'
 set -gx XDG_CONFIG_HOME "$HOME/.config"
 set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -g XDG_DATA_BIN "$HOME/.local/bin"
-set -g FISH_CONFIG_HOME "$XDG_CONFIG_HOME/fish/conf.d"
+set -g FDOTDIR "$XDG_CONFIG_HOME/fish"
 set -x LC_ALL en_US.UTF-8
