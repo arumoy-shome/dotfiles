788014562d79260c2909c2a491a987c92b0189cf
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-02-05 23:27:08 +0100

    [fish]: fix reversed PATH issue in tmux
    Rationale behind changes added in file as comment.

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,14 +1,22 @@
 # NOTE: fish sources config.fish all files under conf.d on startup, thus they
 # get sourced twice if we launch tmux (once when we lauch the terminal and then
-# again when we lauch a tmux session).
+# again when we lauch a tmux session). The fix is simply to manipulate PATH if
+# we are not in tmux.
 
 if test -z "$TMUX"
-  set -l PATHS $HOME/.local/bin \
+# make sure PATHS contains valid entries, this is intentionally left to be done
+# manually for performance (it can be expensive to check if entries are valid if
+# PATHS is long).
+  set -l PATHS /usr/local/opt/coreutils/libexec/gnubin \
     /usr/local/lib/ruby/gems/2.6.0/bin \
     /usr/local/opt/ruby/bin \
-    $HOME/.emacs.d/bin
+    $HOME/.local/bin \
+    $HOME/.emacs.d/bin \
 
-    for P in $PATHS
-      test -d $P; and set PATH $P $PATH
-    end
+  set PATH $PATHS $PATH
 end
+
+# NOTE: tmux runs /usr/libexec/path_helper to build the path which APPENDS our
+# custom paths, the simplest (albeit hacky) way to fix this is in fish is to
+# reverse PATH. See https://scriptingosx.com/2017/05/where-paths-come-from/.
+test -n "$TMUX"; and set PATH $PATH[-1..1]
