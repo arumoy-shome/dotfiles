7f83f8904355587901b0dfac30cb757c8ba51aff
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-02-05 17:49:51 +0100

    [fish]: support args for venv()
    Also a bit or refactoring and better error handling.

--- fish/completions/venv.fish
+++ fish/completions/venv.fish
@@ -1,1 +1,4 @@
-complete -f -c venv -a "activate deactivate create delete"
+complete -f -c venv -s c -l create -d "create a virtualenv"
+complete -f -c venv -s D -l delete -d "delete a virtualenv"
+complete -f -c venv -s a -l activate -d "activate a virtualenv"
+complete -f -c venv -s d -l deactivate -d "deactivate a virtualenv"
--- fish/functions/venv.fish
+++ fish/functions/venv.fish
@@ -1,68 +1,62 @@
 function venv -d "python virtualenv management"
 
-  function _create_venv -d "create a new virtualenv"
-    python3 -m venv venv
+  function _create_venv -a venv -d "create a new virtualenv"
+    test -n "$venv"; or set venv 'venv'
 
-    _activate_venv
-
-    if test -f requirements.txt
-      pip install -r requirements.txt
+    if test ! -d "$venv"
+      python3 -m venv "$venv"; and _activate_venv "$venv"
+      test -f requirements.txt; and pip install -r requirements.txt
+    else
+      echo "Directory $venv already exists."
     end
   end
 
-  function _delete_venv -d "delete virtualenv"
+  function _delete_venv -a venv -d "delete virtualenv"
+    test -n "$venv"; or set venv 'venv'
+
     _deactivate_venv
 
-    if test -d venv
-      rm -rf venv
-    end
-  end
-
-  function _deactivate_venv -d "deactivate current virtualenv"
-    if test -n "$VIRTUAL_ENV"
-      deactivate
-    end
-
-    if test -n "$TMUX"
-      tmux set-environment -u VIRTUAL_ENV
-    end
-  end
-
-  function _activate_venv -d "activate virtualenv if it exists"
-    # fish does not allow command substitution in lists longer than 1
-    # so we have so save the result of the command first
-    set dir_basename (basename (pwd))
-    set venv_names "env/" "venv/" "virtualenv/" "$dir_basename/"
-    set listing (ls -d */ (pwd)) # list only directories
-
-    for name in $venv_names
-      contains $name $listing; and set venv_basename $name; and break
-    end
-
-    if test -n "$venv_basename"
-      source $venv_basename/bin/activate.fish
-      if test -n "$TMUX"
-        tmux set-environment VIRTUAL_ENV $VIRTUAL_ENV
-      end
+    if test -d "$venv"
+      echo "Deleting virtualenv $venv"
+      rm -rf "$venv"
     else
       echo "No virtualenv available in this directory."
     end
   end
 
-  if not test -n "$argv"
-    _activate_venv
-  else
-    switch $argv
-      case create
-        _create_venv
-      case activate
-        _activate_venv
-      case deactivate
-        _deactivate_venv
-      case delete
-        _delete_venv
-      case '*'
-        echo $argv is not a valid argument.
+  function _deactivate_venv -d "deactivate current virtualenv"
+    if test -n "$VIRTUAL_ENV"
+      echo "Deactivating virtualenv $VIRTUAL_ENV."
+      deactivate
+      test -n "$TMUX"; and tmux set-environment -u VIRTUAL_ENV
     end
+  end
+
+  function _activate_venv -a venv -d "activate virtualenv if it exists"
+    set script venv/bin/activate.fish
+    test -n "$venv"; and set script "$venv/bin/activate.fish"
+
+    if test -r "$script"
+      echo "Activating virtualenv $script."
+      source "$script"
+      test -n "$TMUX"; and tmux set-environment VIRTUAL_ENV "$VIRTUAL_ENV"
+    else
+      echo "No virtualenv available in this directory."
+    end
+  end
+
+  argparse --name venv --exclusive 'c,a,d,D' 'c/python3=?' 'a/activate=?' 'd/deactivate' 'D/delete=?' -- $argv
+    or return
+
+  if set -q _flag_c
+    _create_venv "$argv"
+  else if set -q _flag_a
+    _activate_venv "$argv"
+  else if set -q _flag_d
+    _deactivate_venv
+  else if set -q _flag_D
+    _delete_venv "$argv"
+  else
+    _activate_venv
   end
 end
