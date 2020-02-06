c6ab36a9f5919f8b5df71028a1ae7ab176ebed0d
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-02-06 23:32:23 +0100

    [fish]: completion updates.

--- fish/completions/colors.fish
+++ fish/completions/colors.fish
@@ -1,4 +1,3 @@
-# NOTE: this list needs to be manually updated.
 set -l BASE "$FDOTDIR/base16-shell/scripts"
 set -l SCHEMES (find "$BASE" -name 'base16-*.sh' | sed -E 's|.+/base16-||' | sed -E 's/\.sh//')
 complete -f -c colors -a "$SCHEMES help ls"
--- fish/completions/venv.fish
+++ fish/completions/venv.fish
@@ -1,6 +1,9 @@
-set -l venvs (find $PWD -path '*/bin/python' -maxdepth 3 | sed -E 's|/bin/python$||' | sed -E 's@.*/@@')
+# NOTE: the completion options are not cached in a variable by choice since that
+# results in outdated completions being presented.
+# NOTE: `$` in the -a flag argument needs to be escaped due to fish's variable
+# expansion.
 complete -f -c venv -s c -l create -d "create a virtualenv"
-complete -f -c venv -s D -l delete -a "$venvs" -d "delete a virtualenv"
-complete -f -c venv -s a -l activate -a "$venvs" -d "activate a virtualenv"
 complete -f -c venv -s d -l deactivate -d "deactivate a virtualenv"
-complete -f -c venv -a "$venvs" -d "virtualenv"
+complete -f -c venv -s D -l delete -a "(find $PWD -path '*/bin/python' -maxdepth 3 | sed -E 's|/bin/python\$||' | sed -E 's@.*/@@')" -d "delete a virtualenv"
+complete -f -c venv -s a -l activate -a "(find $PWD -path '*/bin/python' -maxdepth 3 | sed -E 's|/bin/python\$||' | sed -E 's@.*/@@')" -d "activate a virtualenv"
+complete -f -c venv -a "(find $PWD -path '*/bin/python' -maxdepth 3 | sed -E 's|/bin/python\$||' | sed -E 's@.*/@@')" -d "virtualenv"
