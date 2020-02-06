f045134198a561675f90f97fb9c42e8504efb876
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-02-06 23:01:32 +0100

    [fish]: performance bump for dot() completion
    Set the -maxdepth flag on find for performance boost and prevent
    recursive search (which is pointless in this case).

--- fish/completions/dot.fish
+++ fish/completions/dot.fish
@@ -1,9 +1,9 @@
 set -l DOTDIR "$HOME/dotfiles"
-test -d "$DOTDIR"; and set -l topics (find $DOTDIR -name '_init' -type f | sed -E 's|/_init$||' | sed -E 's@.*/@@')
+test -d "$DOTDIR"; and set -l topics (find $DOTDIR -name '_init' -type f -maxdepth 2 | sed -E 's|/_init$||' | sed -E 's@.*/@@')
 complete -f -c dot -s h -l help -d 'print a helpful message'
 complete -f -c dot -s L -l ls -d 'list enabled topics'
-complete -f -c dot -s l -l link -a "$topics" -d 'link topics'
-complete -f -c dot -s u -l unlink -a "$topics" -d 'unlink topics'
-complete -f -c dot -s r -l relink -a "$topics" -d 'relink topics'
-complete -f -c dot -s d -l delete -a "$topics" -d 'delete topics'
-complete -f -c dot -a "$topics" -d 'install given topic'
+complete -f -c dot -s l -l link -a "$topics" -d 'link topics' -r
+complete -f -c dot -s u -l unlink -a "$topics" -d 'unlink topics' -r
+complete -f -c dot -s r -l relink -a "$topics" -d 'relink topics' -r
+complete -f -c dot -s d -l delete -a "$topics" -d 'delete topics' -r
+complete -f -c dot -a "$topics" -d 'topic' -r
