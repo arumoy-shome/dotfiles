e56ee54483d2baa5bcfa520bb4a0ce521980a609
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-02-10 19:38:24 +0100

    [fish]: disable base16-shell
    It injects escape sequences into the shell which are used to color and
    style the text however these sequences are also picked up by
    subprocesses (such as fzf) which make their prompts look weird or simply
    cause them to not work. Temporarily disabling until I figure out what
    I want to do next.

--- fish/conf.d/color.fish
+++ fish/conf.d/color.fish
@@ -1,3 +1,5 @@
 # note that we cannot use $XDG_DATA_HOME here since it is not loaded yet
-sh ~/.local/share/base16/current-theme.sh
+# NOTE: base16-shell temporarily disabled since it injects escape sequences
+# that are exported to subprocesses (such as fzf).
+# sh ~/.local/share/base16/current-theme.sh
 test -n "$TMUX"; and sync_tmux_colors
