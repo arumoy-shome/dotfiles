36d8b81426eb13540ea6a7172300cd7384bc240f
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-02-11 13:14:57 +0100

    [fish,kitty]: remove base16-*
    All these colorschemes are not required, it's just fluf.

--- fish/completions/colors.fish
+++
@@ -1,3 +0,0 @@
-set -l BASE "$FDOTDIR/base16-shell/scripts"
-set -l SCHEMES (find "$BASE" -name 'base16-*.sh' | sed -E 's|.+/base16-||' | sed -E 's/\.sh//')
-complete -f -c colors -a "$SCHEMES help ls"
--- fish/conf.d/color.fish
+++
@@ -1,5 +0,0 @@
-# note that we cannot use $XDG_DATA_HOME here since it is not loaded yet
-# NOTE: base16-shell temporarily disabled since it injects escape sequences
-# that are exported to subprocesses (such as fzf).
-# sh ~/.local/share/base16/current-theme.sh
-test -n "$TMUX"; and sync_tmux_colors
