1b190e16f941200db72e03dfc309394bf3095f78
Arumoy Shome <arumoy.shome.work@gmail.com>
2020-02-20 14:09:02 +0100

    [fish]: simplify t()
    I don't need the ssh stuff...

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -7,16 +7,9 @@ function __create_detached_session -S
 end
 
 function t -d "attach to tmux session if it exists, or create a new one with no args envoke tmux when args are passed"
-  # Make sure even pre-existing tmux sessions use the latest SSH_AUTH_SOCK.
-  # (Inspired by: https://gist.github.com/lann/6771001)
-  set -l SOCK_SYMLINK ~/.ssh/ssh_auth_sock
-  if test -r "$SSH_AUTH_SOCK" -a ! -L "$SSH_AUTH_SOCK"
-    ln -sf "$SSH_AUTH_SOCK" "$SOCK_SYMLINK"
-  end
-
   # If provided with args, pass them through.
   if test -n "$argv"
-    env SSH_AUTH_SOCK=$SOCK_SYMLINK tmux "$argv"
+    tmux "$argv"
     return
   end
 
@@ -24,7 +17,7 @@ function t -d "attach to tmux session if it exists, or create a new one with no 
   set -l SESSION_NAME (basename "$PWD" | tr . -)
 
   if test -z "$TMUX"
-    env SSH_AUTH_SOCK=$SOCK_SYMLINK tmux new -A -s "$SESSION_NAME"
+    tmux new -A -s "$SESSION_NAME"
   else
     if not __session_exists; __create_detached_session; end
     tmux switch-client -t "$SESSION_NAME"
