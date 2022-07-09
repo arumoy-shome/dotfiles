d71dea33b657446b8f976267758ea66a0ff9bdad
Arumoy Shome <contact@arumoy.me>
2022-07-09 23:01:28 +0200

    feat: wezterm tmux style bindings

--- .wezterm.lua
+++ .wezterm.lua
@@ -31,25 +31,57 @@ return {
     -- }}}
 
     -- keys {{{
-    -- The following section defines my preferred keybindings.
     -- See
     -- https://wezfurlong.org/wezterm/config/keyboard-concepts.html
     -- for more details, we don't want this since it may interfere
     -- with vim.
     use_dead_keys = false,
+    -- The following section defines my preferred keybindings in
+    -- a tmux-esque manner. I keep the original keybindings for
+    -- backward compatibility and to accomodate muscle memory.
+    leader = {key="Space", mods="CTRL"},
     keys = {
 	-- panes {{{
-	{key="d", mods="CMD", action=action.SplitHorizontal{domain="CurrentPaneDomain"}},
-	{key="d", mods="CMD|SHIFT", action=action.SplitVertical{domain="CurrentPaneDomain"}},
-	{key="w", mods="CTRL|SHIFT", action=action.CloseCurrentPane{confirm=false}},
-	{key="z", mods="CTRL|SHIFT", action=action.TogglePaneZoomState},
+	{key="v", mods="LEADER", action=action.SplitHorizontal{domain="CurrentPaneDomain"}},
+	{key="s", mods="LEADER", action=action.SplitVertical{domain="CurrentPaneDomain"}},
+	{key="s", mods="LEADER|SHIFT", action=action.SplitPane{
+	    direction="Down",
+	    size={Percent=25},
+	}},
+	{key="x", mods="LEADER", action=action.CloseCurrentPane{confirm=false}},
+	{key="z", mods="LEADER", action=action.TogglePaneZoomState},
+	{key="h", mods="LEADER", action=action.ActivatePaneDirection("Left")},
+	{key="j", mods="LEADER", action=action.ActivatePaneDirection("Down")},
+	{key="k", mods="LEADER", action=action.ActivatePaneDirection("Up")},
+	{key="l", mods="LEADER", action=action.ActivatePaneDirection("Right")},
 	--- }}}
+	-- tabs {{{
+	{key="p", mods="LEADER", action=action.ActivateTabRelative(-1)},
+	{key="n", mods="LEADER", action=action.ActivateTabRelative(1)},
+	{key="c", mods="LEADER", action=action.SpawnTab("DefaultDomain")},
+	{key="w", mods="LEADER", action=action.CloseCurrentTab{confirm=false}},
+	{key="o", mods="LEADER", action=action.ActivateLastTab},
+	-- }}}
+	-- copy mode {{{
+	{key="Return", mods="LEADER", action=action.ActivateCopyMode},
+	{key=" ", mods="LEADER", action=action.QuickSelect},
+	-- }}}
 	-- shell integration {{{
 	{key="UpArrow", mods="SHIFT", action=action.ScrollToPrompt(-1)},
 	{key="DownArrow", mods="SHIFT", action=action.ScrollToPrompt(1)},
 	-- }}}
     },
     -- }}}
+
+    -- mouse {{{
+    mouse_bindings = {
+	{
+	    event={Up={streak=1, button="Left"}},
+	    mods="CMD",
+	    action=action.OpenLinkAtMouseCursor,
+	},
+    },
+    -- {{{
 
     -- font {{{
     font_size = 16,
@@ -66,6 +98,10 @@ return {
     -- status bar {{{
     -- Update every 6000 milliseconds or 1 minute.
     status_update_interval = 6000,
+    -- }}}
+
+    -- panes {{{
+    unzoom_on_switch_pane = true, -- default
     -- }}}
 
 }
